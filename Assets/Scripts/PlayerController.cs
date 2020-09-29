5138ce5b8ca27d67e0e78a91e306527c0be6bf14
Alexandre BREUX <breux.alexandre@gmail.com>
2020-09-29 17:10:57 +0200

    Project creation

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerMovement playerMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+            playerMovement.MoveX(Input.GetAxis("Horizontal"));
+        if (Input.GetButtonDown("Jump"))
+            playerMovement.Jump();
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Propriété physisque")]
+    [SerializeField]
+    private float speedXMax = 0.5f;
+    [SerializeField]
+    private float jumpHeigh = 1f;
+    private bool grounded;
+    [SerializeField]
+    private float gravity;
+    private float speedY;
+    [SerializeField]
+    private float speedYMin; // vitesse de chute maximum (minimum car négative)
+    [SerializeField]
+    private int maxNumberOfJump;
+    private int remainJump;
+
+    [Header("GameObject")]
+    [SerializeField]
+    private Transform bottomLeft;
+    [SerializeField]
+    private Transform topRight;
+
+    public void MoveX(float input)
+    {
+        transform.position = transform.position + new Vector3(input
+            , 0
+            , 0) * speedXMax * Time.deltaTime;
+    }
+
+
+    public void Jump()
+    {
+        if (remainJump > 0)
+        {
+            speedY = jumpHeigh;
+            grounded = false;
+            remainJump--;
+        }
+    }
+
+    void Update()
+    {
+        ComputeSpeed();
+        transform.position += new Vector3(0, speedY, 0) * Time.deltaTime;
+        CheckCollision();
+    }
+
+    private void CheckCollision()
+    {
+        Collider2D checkCollid = Physics2D.OverlapArea(bottomLeft.position,topRight.position);
+        if(checkCollid != null && checkCollid.CompareTag("sol"))
+        {
+            Debug.Log("Collision" + checkCollid.name);
+            grounded = true;
+            remainJump = maxNumberOfJump;
+        }
+        else
+            grounded = false;
+    }
+
+    private void ComputeSpeed()
+    {
+        if (grounded)
+            speedY = 0;
+        else
+        {
+            if(speedY > speedYMin)
+            {
+                speedY -= gravity;
+            }
+        }
+    }
+
+}
