c92d75b0ff10451eab0a9b2e54ab04004f3bca9b
Louis <louis.ginesy@telecom-sudparis.eu>
2020-10-03 22:40:28 +0200

    Collisions + Plateforme 1way + gravity
    Ajout d'une premiere version des collisions avec les murs et le plafond + une plateforme que l'on peut passer dans un sens mais pas l'autre + changement de la gravité pour avoir des sauts plus hauts

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,7 +10,6 @@ public class PlayerMovement : MonoBehaviour
     private float speedXMax = 0.5f;
     [SerializeField]
     private float jumpHeigh = 1f;
-    private bool grounded;
     [SerializeField]
     private float gravity;
     private float speedY;
@@ -20,17 +19,42 @@ public class PlayerMovement : MonoBehaviour
     private int maxNumberOfJump;
     private int remainJump;
 
-    [Header("GameObject")]
+
+    private bool grounded;
+    private bool rightWalled;
+    private bool leftWalled;
+
+    [Header("GameObject - Ground Detectors")]
     [SerializeField]
-    private Transform bottomLeft;
+    private Transform GBottomLeft;
     [SerializeField]
-    private Transform topRight;
+    private Transform GTopRight;
+    [Header("GameObject - RightWall Detectors")]
+    [SerializeField]
+    private Transform RWBottomLeft;
+    [SerializeField]
+    private Transform RWTopRight;
+    [Header("GameObject - RightWall Detectors")]
+    [SerializeField]
+    private Transform LWBottomLeft;
+    [SerializeField]
+    private Transform LWTopRight;
+    [Header("GameObject - Ceilling Detectors")]
+    [SerializeField]
+    private Transform CBottomLeft;
+    [SerializeField]
+    private Transform CTopRight;
 
     public void MoveX(float input)
     {
-        transform.position = transform.position + new Vector3(input
+        float speedX = input * speedXMax * Time.deltaTime;
+        if (speedX > 0 && rightWalled)
+            speedX = 0;
+        else if (speedX < 0 && leftWalled)
+            speedX = 0;
+        transform.position = transform.position + new Vector3(speedX
             , 0
-            , 0) * speedXMax * Time.deltaTime;
+            , 0);
     }
 
 
@@ -46,25 +70,55 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        ComputeSpeed();
+        ComputeSpeedY();
         transform.position += new Vector3(0, speedY, 0) * Time.deltaTime;
-        CheckCollision();
+        CheckAllCollisions();
     }
 
-    private void CheckCollision()
+    private void CheckAllCollisions()
     {
-        Collider2D checkCollid = Physics2D.OverlapArea(bottomLeft.position,topRight.position);
-        if(checkCollid != null && checkCollid.CompareTag("sol"))
+        Collider2D checkCollid = Physics2D.OverlapArea(GBottomLeft.position,GTopRight.position);
+
+        // CheckGrounded
+        if(checkCollid != null && (checkCollid.CompareTag("Solide") || checkCollid.CompareTag("Holographique")))
         {
-            Debug.Log("Collision" + checkCollid.name);
+            Debug.Log("Collision Ground : " + checkCollid.name);
             grounded = true;
             remainJump = maxNumberOfJump;
         }
         else
             grounded = false;
+
+        // CheckRightWalled
+        checkCollid = Physics2D.OverlapArea(RWBottomLeft.position, RWTopRight.position);
+        if (checkCollid != null && checkCollid.CompareTag("Solide"))
+        {
+            Debug.Log("Collision Right Wall : " + checkCollid.name);
+            rightWalled = true;
+        }
+        else
+            rightWalled = false;
+
+        // CheckLeftWalled
+        checkCollid = Physics2D.OverlapArea(LWBottomLeft.position, LWTopRight.position);
+        if (checkCollid != null && checkCollid.CompareTag("Solide"))
+        {
+            Debug.Log("Collision Left Wall : " + checkCollid.name);
+            leftWalled = true;
+        }
+        else
+            leftWalled = false;
+
+        // CheckCeilling
+        checkCollid = Physics2D.OverlapArea(CBottomLeft.position, CTopRight.position);
+        if (checkCollid != null && checkCollid.CompareTag("Solide"))
+        {
+            Debug.Log("Collision Ceilling : " + checkCollid.name);
+            speedY = 0;
+        }
     }
 
-    private void ComputeSpeed()
+    private void ComputeSpeedY()
     {
         if (grounded)
             speedY = 0;
