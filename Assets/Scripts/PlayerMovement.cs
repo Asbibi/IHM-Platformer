72639ce9dadab4e764ba985b59ce4918d5b4a754
Louis <louis.ginesy@telecom-sudparis.eu>
2020-10-10 16:54:17 +0200

    Petites modifs sur le mouvement
    Modification du freinage en walljump + annulation de jumpSpeedX lorsque le joueur retouche un mur + augmentation de la distance de detection pour pas rentrer dans les murs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
-using System;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Propriété physisque")]
+    [Header("Speeds and Forces")]
     private float speedX;
     private float moveSpeedX;
     private float jumpSpeedX;
@@ -16,15 +14,15 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float speedYMin = -10; // vitesse de chute maximum (minimum car négative)
     [SerializeField]
-    private float jumpHeight = 1f;
-    [SerializeField]
-    private float wallJumpSpeed = 1f;
-
-    
+    private float jumpSpeedYInit = 1f;    
     [SerializeField]
     private float jumpSpeedXMax = 15f;
+
+    [Header("Frictions and Jump Parameters")]
     [SerializeField]
     private float gravity = 0.3f;
+    [SerializeField]
+    private float wallJumpGravity = 0.1f;
     [SerializeField]
     private float friction = 1f;
     [SerializeField]
@@ -44,7 +42,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float tolerance = 0.01f;
 
 
-    // Unity Methods
+    // ===================== Unity Methods =====================
     private void Start()
     {
         collider2d = GetComponent<BoxCollider2D>();
@@ -53,13 +51,13 @@ public class PlayerMovement : MonoBehaviour
     {
         ComputeSpeedY();
         ComputeSpeedX();
+        Debug.Log("SpeedX : " + speedX + " = " + moveSpeedX + " + " + jumpSpeedX);
         CheckAllCollisions();
         transform.position += new Vector3(speedX, speedY, 0) * Time.deltaTime;
-        //CheckAllCollisionsOld();
     }
 
 
-    // Public Methods
+    // ===================== Public Methods =====================
     public void MoveX(float input)
     {
         moveSpeedX = input * speedXMax;
@@ -72,7 +70,7 @@ public class PlayerMovement : MonoBehaviour
     { 
         //Walljump
         if((leftWalled ||rightWalled) && !grounded){
-            speedY = jumpHeight;
+            speedY = jumpSpeedYInit;
             jumpSpeedX = leftWalled ? jumpSpeedXMax : -jumpSpeedXMax; //Pousse du mur
             leftWalled = !leftWalled;
             rightWalled = !rightWalled;
@@ -81,12 +79,14 @@ public class PlayerMovement : MonoBehaviour
         
         else if (remainJump > 0)
         {
-            speedY = jumpHeight;
+            speedY = jumpSpeedYInit;
             grounded = false;
             remainJump--;
         }
     }
 
+
+    // ===================== Collisions =====================
     private void CheckAllCollisions()
     {
         RaycastHit2D[] _results = new RaycastHit2D[5];
@@ -151,7 +151,7 @@ public class PlayerMovement : MonoBehaviour
             for (int i = 0; i < _nbResult; i++)
             {
                 RaycastHit2D _rch2d = _results[i];
-                if (_rch2d.collider != null && (_rch2d.collider.CompareTag("Solide") || _rch2d.collider.CompareTag("Holographique")) && (_rch2d.distance < _nearestDistance))
+                if (_rch2d.collider != null && _rch2d.collider.CompareTag("Solide") && (_rch2d.distance < _nearestDistance))
                     _nearestDistance = _rch2d.distance;
             }
 
@@ -159,6 +159,7 @@ public class PlayerMovement : MonoBehaviour
             {
                 rightWalled = true;
                 speedX = 0;
+                jumpSpeedX = 0;
                 transform.position += new Vector3(_nearestDistance - tolerance, 0, 0);
             }
         }
@@ -174,7 +175,7 @@ public class PlayerMovement : MonoBehaviour
             for (int i = 0; i < _nbResult; i++)
             {
                 RaycastHit2D _rch2d = _results[i];
-                if (_rch2d.collider != null && (_rch2d.collider.CompareTag("Solide") || _rch2d.collider.CompareTag("Holographique")) && (_rch2d.distance < _nearestDistance))
+                if (_rch2d.collider != null && _rch2d.collider.CompareTag("Solide") && (_rch2d.distance < _nearestDistance))
                     _nearestDistance = _rch2d.distance;
             }
 
@@ -182,6 +183,7 @@ public class PlayerMovement : MonoBehaviour
             {
                 leftWalled = true;
                 speedX = 0;
+                jumpSpeedX = 0;
                 transform.position += new Vector3(tolerance - _nearestDistance, 0, 0);
             }
         }
@@ -189,52 +191,8 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
-    /*
-    private void CheckAllCollisionsOld()
-    {
-        Collider2D checkCollid = Physics2D.OverlapArea(GBottomLeft.position,GTopRight.position);
 
-        // CheckGrounded
-        if(checkCollid != null && (checkCollid.CompareTag("Solide") || checkCollid.CompareTag("Holographique")))
-        {
-            Debug.Log("Collision Ground : " + checkCollid.name);
-            grounded = true;
-            remainJump = maxNumberOfJump;
-        }
-        else
-            grounded = false;
-
-        // CheckRightWalled
-        checkCollid = Physics2D.OverlapArea(RWBottomLeft.position, RWTopRight.position);
-        if (checkCollid != null && checkCollid.CompareTag("Solide"))
-        {
-            Debug.Log("Collision Right Wall : " + checkCollid.name);
-            rightWalled = true;
-        }
-        else
-            rightWalled = false;
-
-        // CheckLeftWalled
-        checkCollid = Physics2D.OverlapArea(LWBottomLeft.position, LWTopRight.position);
-        if (checkCollid != null && checkCollid.CompareTag("Solide"))
-        {
-            Debug.Log("Collision Left Wall : " + checkCollid.name);
-            leftWalled = true;
-        }
-        else
-            leftWalled = false;
-
-        // CheckCeilling
-        checkCollid = Physics2D.OverlapArea(CBottomLeft.position, CTopRight.position);
-        if (checkCollid != null && checkCollid.CompareTag("Solide"))
-        {
-            Debug.Log("Collision Ceilling : " + checkCollid.name);
-            speedY = 0;
-        }
-    }*/
-
-
-    // Compute Speed Methods
+    // ===================== Compute Speed Methods =====================
     private void ComputeSpeedY()
     {
         if (grounded)
@@ -243,8 +201,8 @@ public class PlayerMovement : MonoBehaviour
         {
             if(speedY > speedYMin)
             {
-                if(leftWalled || rightWalled)
-                    speedY -= gravity/3;
+                if((leftWalled || rightWalled) && (speedY <= 0))
+                    speedY -= wallJumpGravity;
                 else
                     speedY -= gravity;
             }
