4af6a35dab695f74e04d4591ac74073d55f28d58
Alexandre BREUX <breux.alexandre@gmail.com>
2020-10-30 11:12:19 +0100

    Add placeholder for tutorial

---
+++ Assets/SpriteChange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Sprites;
+
+public class SpriteChange : MonoBehaviour
+{
+    public Sprite[] sprites;
+    [SerializeField]
+    private SpriteRenderer FirstPlaceHolder;
+    [SerializeField]
+    private SpriteRenderer SecondPlaceHolder;
+    [SerializeField]
+    private SpriteRenderer ThirdPlaceHolder;
+    [SerializeField]
+    private SpriteRenderer FourthPlaceHolder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
