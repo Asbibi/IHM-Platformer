1f81008c84aabfac97f5ccddc0cb003d6b295a89
Alexandre BREUX <breux.alexandre@gmail.com>
2020-10-31 17:00:07 +0100

    Add chrono (basic)

---
+++ Assets/Scripts/UITimeManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITimeManager : MonoBehaviour
+{
+    private Text chrono;
+    private float elapsedTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        chrono = gameObject.GetComponent<Text>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        chrono.text = ((int)elapsedTime).ToString();
+    }
+}
